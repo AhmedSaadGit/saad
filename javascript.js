c0160540c37631a5a320b55514ef41e934359c16
AhmedSaadGit <76616865+AhmedSaadGit@users.noreply.github.com>
2021-02-09 22:02:01 +0100

    the last javascript

--- javascript.js
+++ javascript.js
@@ -1,15 +1,22 @@
-console.log("hhh");
 
 function showCity(){
     let villeCourante = document.getElementById("choose").value;
     let cities = document.getElementsByClassName("city");
-  /*  
     for(i=0;i<cities.length;i++){
         let show = document.getElementById(cities[i].id);
-    if(villeCourante == cities[i].id){
-        show.style.display="none"; 
+
+    if(villeCourante == " "){
+        show.style.display="";
+        
     }
+    else if(villeCourante == cities[i].id){
         show.style.display=""; 
-}*/
+        
+    }
+     else{ 
+        show.style.display="none";
+        
+    }
+}
 
 }
