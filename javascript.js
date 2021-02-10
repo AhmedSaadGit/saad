30e23614037e5b53add9a1c9fb4a7e7f2dcb4c9b
AhmedSaadGit <76616865+AhmedSaadGit@users.noreply.github.com>
2021-02-10 19:46:53 +0100

    The last man
    lol

--- javascript.js
+++ javascript.js
@@ -1,22 +1,32 @@
+function begin(){
+    let cities = document.getElementsByClassName("city");
+    for(i=0;i<cities.length;i++){
+    let show = document.getElementById(cities[i].id);
+    show.style.display="none";}
+}
+
 
 function showCity(){
     let villeCourante = document.getElementById("choose").value;
     let cities = document.getElementsByClassName("city");
+    
     for(i=0;i<cities.length;i++){
         let show = document.getElementById(cities[i].id);
-
-    if(villeCourante == " "){
-        show.style.display="";
         
+    if(villeCourante == "all"){
+        show.style.display="";
+        selected = "Every";
     }
     else if(villeCourante == cities[i].id){
         show.style.display=""; 
-        
+        selected = villeCourante;
     }
      else{ 
-        show.style.display="none";
-        
+        show.style.display="none";  
     }
+
 }
+
+document.getElementById("selectedcity").innerHTML = "You Have Choosed "+ selected +" city";
 
 }
